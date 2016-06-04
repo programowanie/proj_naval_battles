aa6d88ed9e7e5f8036cbfec68fa6dc44c2b25a7b
226100 <piotrchlystek@hotmail.com>
2016-06-04 22:29:57 +0200

    ver. 0.07

--- main.cpp
+++ main.cpp
@@ -11,14 +11,20 @@
 
 bool read(const string &file_name, Parameters tab[]);
 
+void showParameters(Parameters tab[], int n);
+
 int main(int argc, char *argv[]) {
     if (argc == 1) srand(atoi(argv[1]));
     else srand(time(NULL));
 
     Parameters *parametersUSA = new Parameters[1];
-    //Parameters *parametersJAPAN = new Parameters[15];
+    Parameters *parametersJAPAN = new Parameters[15];
+
     if (read("USA.dat", parametersUSA))
-        cout << "\nPoprawnie wczytano parametry statkow\nWyswietlic wczytane parametry? [t/n]: " << endl;
+        cout << "\nPoprawnie wczytano parametry statkow\nWyswietlic wczytane parametry? [t/n]: ";
+    char whatToDo;
+    cin >> whatToDo;
+    if (whatToDo == 't') showParameters(parametersUSA, 1);
 
     //read("JAPAN.dat", parametersJAPAN);
 
@@ -27,18 +33,30 @@ int main(int argc, char *argv[]) {
     Battle battle;
 
     delete[] parametersUSA;
-    //delete [] parametersJAPAN;
+    delete[] parametersJAPAN;
+
+    cin.sync();
+    cin.get();
 
     return 0;
 }
 
-bool read(string file_name, Parameters tab[]) {
+bool read(const string &file_name, Parameters tab[]) {
+    int typeOfWarship;
     ifstream file;
     file.open(file_name.c_str());
     if (!file.good()) return false;
 
     for (int i = 0; i < 1; i++) {
         file >> tab[i].name;
+
+        file >> typeOfWarship;
+        if (typeOfWarship == 0) tab[i].type = DESTROYER;
+        else if (typeOfWarship == 1) tab[i].type = CRUISER;
+        else if (typeOfWarship == 2) tab[i].type = BATTLESHIP;
+        else if (typeOfWarship == 4) tab[i].type = AIRCRAFTCARRIER;
+
+
         file >> tab[i].baseHp;
         file >> tab[i].armor;
         file >> tab[i].agility;
@@ -59,4 +77,46 @@ bool read(string file_name, Parameters tab[]) {
         file >> tab[i].dmgPerSquadron;
         file >> tab[i].baseHpPerSquadron;
     }
+
+    return true;
 }
+
+void showParameters(Parameters *tab, int n) {
+    for (int i = 0; i < 1; i++) {
+        cout << "\n#### Numer statku: " << i + 1 << endl;
+        cout << "\nNazwa: " << tab[i].name << endl;
+
+        cout << "Typ: ";
+        if (tab[i].type == DESTROYER) cout << "niszczyciel" << endl;
+        else if (tab[i].type == CRUISER) cout << "krazownik" << endl;
+        else if (tab[i].type == BATTLESHIP) cout << "pancernik" << endl;
+        else cout << "lotniskowiec" << endl;
+
+        cout << "HP: " << tab[i].baseHp << endl;
+        cout << "Pancerz: " << tab[i].armor << endl;
+        cout << "Zwrotnosc: " << tab[i].agility << endl;
+        cout << "Kamuflaz: " << tab[i].camouflage << endl;
+        cout << "Szansa na podpalenie: " << tab[i].chanceForArson << endl;
+
+        cout << "\n## Obrona \n" << endl;
+
+        cout << "Liczba dzial przeciwlotniczych: " << tab[i].amountOfAntiAircraftCannons << endl;
+        cout << "Max. dmg na dzialo: " << tab[i].maxAntiAircraftCannonsDmg << endl;
+
+        cout << "\n## Atak \n" << endl;
+
+        cout << "Liczba dzial: " << tab[i].amountOfCannons << endl;
+        cout << "Dmg pocisku HE: " << tab[i].maxHeShellDmg << endl;
+        cout << "Dmg pocisku AP: " << tab[i].maxApShellDmg << endl;
+        cout << "Zasieg ostrzalu: " << tab[i].firingRange << endl;
+        cout << "Szansa na podpalenie pociskiem HE: " << tab[i].chanceForArsonByHe << endl;
+        cout << "Liczba torped: " << tab[i].amountOfTorpedos << endl;
+        cout << "Dmg torpedy: " << tab[i].maxTorpedoDmg << endl;
+        cout << "Zasieg torpedy: " << tab[i].torpedoRange << endl;
+        cout << "Liczba eskadr: " << tab[i].amountOfSquadrons << endl;
+        cout << "Liczba samolotow w eskadrze: " << tab[i].aircraftInSquadron << endl;
+        cout << "Dmg zadawany przez eskadre: " << tab[i].dmgPerSquadron << endl;
+        cout << "HP eskadry: " << tab[i].baseHpPerSquadron << endl;
+    }
+}
+
